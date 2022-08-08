7dfc7105e40fe45157d8e108ac32fb31ced61382
amaile <ajamaile@gmail.com>
2022-08-08 14:47:50 -0700

    added function to get newSequence - not sure it works yet

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ function initGame() {
     simonSequence = null;
     simonTurn = true;
     updateMessage.textContent = 'Try to keep up!';
+    newRound();
 }
 playButton.addEventListener('click', initGame);
 
@@ -21,14 +22,15 @@ playButton.addEventListener('click', initGame);
 // Creating function for new click by simon, needs to be random choice, rounded number within board length. 
 // Now the players turn 
 // Pushing new click to simon's sequence 
-const boards = ['one', 'two', 'three', 'four']; 
+
 
 function newClick() {
+    const boards = ['one', 'two', 'three', 'four']; 
     let boardClick = boards[Math.floor(Math.random() * boards.length)];
-    playerTurn = true;
     return boardClick;
-}
-simonSequence.push(newClick());
+};
+
+
 
 
 // figured out how to get all these event listeners into one function yay! 
@@ -45,3 +47,15 @@ let buttons = document.querySelectorAll('.game-button');
     });
 };  
 colorChange();
+
+
+
+//Hoping this will keep track of new sequence each round
+function newSequence(i) {
+    if (i = 0, i < simonSequence.length, i++) {
+        colorChange(simonSequence[i]);
+        setTimeout(function() {
+            newSequence(i);
+        }, 400);
+    };
+};
