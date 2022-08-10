5c3954bb7c8159f167043d9c6e1980ce5f9a6d80
amaile <ajamaile@gmail.com>
2022-08-10 10:23:30 -0700

    computer starts! but only once

--- js/main.js
+++ js/main.js
@@ -28,22 +28,24 @@ function initGame() {
  
 
 // simon starts a new round
-// newRound calls newClick and 
+// newRound calls newClick and showMoves
 function newRound() {
     let newSequence = [simonSequence.push(newClick())];
     playerTurn = true;
-    return newSequence;
+    showMoves(newSequence[simonSequence.length - 1]);
 };
-showMoves(board);
+
 
 
 
 // calling for newClick in each newRound  
 // i think i was making this too complicated so simplified but not sure if it still will work
 function newClick() {
-    let randomClick = [Math.floor(Math.random() * 4)];
+    let randomClick = (Math.floor(Math.random() * 4));
     return randomClick;
 };
+
+
 
 // great idea from kenny in office hours 
 // I want to call this function above in newround but it won't let me. Off to take a break.
@@ -51,46 +53,45 @@ function showMoves(board) {
     
     switch (board){
         case 0:
-              document.getElementById('0').addClass("active");
+              document.getElementById('0').classList.add("active");
               setTimeout(function(){
-                document.getElementById('0').removeClass("active");
-              },200)
+                document.getElementById('0').classList.remove("active");
+              },300)
               break;
         case 1:
-            document.getElementById('1').addClass("active");
+            document.getElementById('1').classList.add("active");
               setTimeout(function(){
-                document.getElementById('1').removeClass("active");
-              },200)
+                document.getElementById('1').classList.remove("active");
+              },300)
               break;
         case 2:
-            document.getElementById('2').addClass("active");
+            document.getElementById('2').classList.add("active");
               setTimeout(function(){
-                document.getElementById('2').removeClass("active");
-              },200)
+                document.getElementById('2').classList.remove("active");
+              },300)
               break;
         case 3:
-            ddocument.getElementById('3').addClass("active");
+            ddocument.getElementById('3').classList.add("active");
             setTimeout(function(){
-              document.getElementById('3').removeClass("active");
-            },200)
+              document.getElementById('3').classList.remove("active");
+            },300)
             break;
     }
  };
 
 
 
-//Might still need this code below for player?
 
-// // figured out how to get all these event listeners into one function yay! 
-// // will need to call this function in play game
-// function colorChange() {
-//     let buttons = document.querySelectorAll('.game-button');
-//     buttons.forEach(function(item) {
-//         item.addEventListener('click', function onClick() {
-//             item.style.backgroundColor = 'white';
-//              setTimeout(function() {
-//                 item.style.backgroundColor = '';
-//             }, 200);
-//         });
-//     });
-// }; 
+// Going to call this in playerRound
+function colorChange() {
+    let buttons = document.querySelectorAll('.game-button');
+    buttons.forEach(function(item) {
+        item.addEventListener('click', function onClick() {
+            item.style.backgroundColor = 'white';
+             setTimeout(function() {
+                item.style.backgroundColor = '';
+            }, 200);
+        });
+    });
+}; 
+colorChange();
